2453c91571484e40132c6688989376d992c2a8a2
mkordik <kordikmarek@davincisoftware.sk>
2016-07-15 19:31:44 +0200

    testing properties

--- src/main/java/sk/thenoen/rasptemp/TestBean.java
+++ src/main/java/sk/thenoen/rasptemp/TestBean.java
@@ -2,6 +2,7 @@ package sk.thenoen.rasptemp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +10,12 @@ public class TestBean {
 
     private static final Logger log = LoggerFactory.getLogger(TestBean.class);
 
+	@Value("${testbean.configparam}")
+	private String configParam;
+
     public void sayHi() {
         log.info("Hi!");
+		log.info("value of config param: '{}'", configParam);
     }
 
 }
