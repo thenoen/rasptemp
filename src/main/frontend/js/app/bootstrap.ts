7c40360a91ff7acf99d846e432b5671d4d514351
mkordik <kordikmarek@davincisoftware.sk>
2016-07-17 00:05:52 +0200

    attempts to integrate spring-boot and angular2 frontend - 2

--- src/main/frontend/js/app/bootstrap.ts
+++ src/main/frontend/js/app/bootstrap.ts
@@ -1,3 +1,5 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
+///<reference path="../../../../../node_modules/angular2/typings/browser.d.ts"/>
+
+import {bootstrap} from '../../../../../node_modules/@angular/platform-browser-dynamic';
 import {PonyRacerApp} from './rasptemp.component';
 bootstrap(PonyRacerApp).catch(err => console.log(err)); // useful to catch the errors
