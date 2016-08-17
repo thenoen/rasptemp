571054d4705dba71a49c8978bf033a06a7b2a9fc
marek <kordikmarek@gmail.com>
2016-08-17 23:14:29 +0200

    solved controller test - one is failing

--- server/src/test/java/sk/thenoen/rasptemp/controller/IndexControllerTest.java
+++ server/src/test/java/sk/thenoen/rasptemp/controller/IndexControllerTest.java
@@ -6,9 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcBuilderCustomizer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import sk.thenoen.rasptemp.RaspTempApplication;
 import sk.thenoen.rasptemp.temperature.TemperatureRecordLoadingService;
 import sk.thenoen.rasptemp.web.StatisticsController;
@@ -19,10 +23,6 @@ import sk.thenoen.rasptemp.web.StatisticsController;
 @AutoConfigureMockMvc
 public class IndexControllerTest {
 
-//	@Autowired
-//	private TestRestTemplate restTemplate;
-
-	@Autowired
 	private MockMvc mvc;
 
 	@Autowired
@@ -32,8 +32,9 @@ public class IndexControllerTest {
 	private TemperatureRecordLoadingService temperatureRecordLoadingService;
 
 	@Test
-	public void exampleTest() {
-//		String body = this.restTemplate.getForObject("/", String.class);
+	public void exampleTest() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new StatisticsController()).build();
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get("/latestValue")).andReturn();
 		Assert.assertEquals("body", "Hello World");
 	}
 }
