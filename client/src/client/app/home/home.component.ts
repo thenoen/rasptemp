235b9372475b41144c4143a8d9d194a86644a81a
marek <kordikmarek@gmail.com>
2016-10-02 15:32:46 +0200

    updated to angular 2.0.0 - build still fails

--- client/src/client/app/home/home.component.ts
+++ client/src/client/app/home/home.component.ts
@@ -9,7 +9,7 @@ import {ChartComponent} from '../chart';
   moduleId: module.id,
   selector: 'sd-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css']
+  styleUrls: ['home.component.css'],
 })
 
 export class HomeComponent implements OnInit {
