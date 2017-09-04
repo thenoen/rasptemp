b81cc983966f59f1f761c60f2f20fe51967fe75a
marek <kordikmarek@gmail.com>
2017-09-04 23:20:16 +0200

    solved adding of new data series to chart

--- client/src/app/a2-highcharts-graph/a2-highcharts-graph.component.ts
+++ client/src/app/a2-highcharts-graph/a2-highcharts-graph.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartComponent } from 'angular2-highcharts';
+import { ChartObject } from 'highcharts'
+// import {HighchartsChartObject} from 'angular2-highcharts';
 
 @Component({
   selector: 'app-a2-highcharts-graph',
@@ -7,36 +9,41 @@ import { ChartComponent } from 'angular2-highcharts';
   styleUrls: ['./a2-highcharts-graph.component.css']
 })
 export class A2HighchartsGraphComponent implements OnInit {
+
+  options: Object;
+  chart: ChartObject;
+
   constructor() {
     this.options = {
       chart: {
         type: 'spline'
       },
       title: { text: 'angular2-highcharts example' },
-      series: [{
-        name: 'temperatures',
-        data: [
-          [Date.UTC(1970, 9, 21), 0],
-          [Date.UTC(1970, 10, 4), 0.28],
-          [Date.UTC(1970, 10, 9), 0.25],
-          [Date.UTC(1970, 10, 27), 0.2],
-          [Date.UTC(1970, 11, 2), 0.28],
-          [Date.UTC(1970, 11, 26), 0.28],
-          [Date.UTC(1970, 11, 29), 0.47],
-          [Date.UTC(1971, 0, 11), 0.79],
-          [Date.UTC(1971, 0, 26), 0.72],
-          [Date.UTC(1971, 1, 3), 1.02],
-          [Date.UTC(1971, 1, 11), 1.12],
-          [Date.UTC(1971, 1, 25), 1.2],
-          [Date.UTC(1971, 2, 11), 1.18],
-          [Date.UTC(1971, 3, 11), 1.19],
-          [Date.UTC(1971, 4, 1), 1.85],
-          [Date.UTC(1971, 4, 5), 2.22],
-          [Date.UTC(1971, 4, 19), 1.15],
-          [Date.UTC(1971, 5, 3), 0]
-        ],
-        allowPointSelect: true
-      }
+      series: [
+        {
+          name: 'temperatures',
+          data: [
+            [Date.UTC(1970, 9, 21), 0],
+            [Date.UTC(1970, 10, 4), 0.28],
+            [Date.UTC(1970, 10, 9), 0.25],
+            [Date.UTC(1970, 10, 27), 0.2],
+            [Date.UTC(1970, 11, 2), 0.28],
+            [Date.UTC(1970, 11, 26), 0.28],
+            [Date.UTC(1970, 11, 29), 0.47],
+            [Date.UTC(1971, 0, 11), 0.79],
+            [Date.UTC(1971, 0, 26), 0.72],
+            [Date.UTC(1971, 1, 3), 1.02],
+            [Date.UTC(1971, 1, 11), 1.12],
+            [Date.UTC(1971, 1, 25), 1.2],
+            [Date.UTC(1971, 2, 11), 1.18],
+            [Date.UTC(1971, 3, 11), 1.19],
+            [Date.UTC(1971, 4, 1), 1.85],
+            [Date.UTC(1971, 4, 5), 2.22],
+            [Date.UTC(1971, 4, 19), 1.15],
+            [Date.UTC(1971, 5, 3), 0]
+          ],
+          allowPointSelect: true
+        }
         // , {
         //   name: 's2',
         //   data: [-2, -3, -5, -8, -13],
@@ -63,8 +70,9 @@ export class A2HighchartsGraphComponent implements OnInit {
       }
     };
   }
-  options: Object;
-  chart: ChartComponent;
+
+
+  test: ChartObject;
   saveChart(chart) {
     console.log(chart)
     this.chart = chart;
@@ -73,17 +81,42 @@ export class A2HighchartsGraphComponent implements OnInit {
     console.log(this.chart);
     var point: any;
     point = [Date.UTC(1971, 12, 3), 5];
-    this.chart.series[0].addPoint(point);
+    // this.chart.series[0].addPoint(point);
     // this.chart.series[1].addPoint(Math.random() * -10);
+
+    this.changeSeries();
   }
+
   onPointSelect(point) {
     alert(`${point.y} is selected`);
   }
+
   onSeriesHide(series) {
     alert(`${series.name} is selected`);
   }
 
   ngOnInit() {
+  }
+
+  changeSeries() {
+
+    console.log("this.chart:")
+    console.log(this.chart)
+    console.log("this.chart.addSeries:")
+    console.log(this.chart.addSeries)
+
+    this.chart.addSeries({
+      name: 's1',
+      data: [[Date.UTC(1970, 10, 9), 1.25],
+      [Date.UTC(1970, 10, 27), 1.2],
+      [Date.UTC(1970, 11, 2), 1.28],
+      [Date.UTC(1970, 11, 26), 1.28],
+      [Date.UTC(1970, 11, 29), 1.47],
+      [Date.UTC(1971, 0, 11), 1.79],
+      [Date.UTC(1971, 0, 26), 1.72]],
+      allowPointSelect: true
+    })
+
   }
 
 }
