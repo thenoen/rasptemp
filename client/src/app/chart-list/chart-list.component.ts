6da427481300e4bf3eb10926629385d0d92b3bb3
marek <kordikmarek@gmail.com>
2017-09-07 21:59:31 +0200

    solved adding of multipl instances of chart

--- client/src/app/chart-list/chart-list.component.ts
+++ client/src/app/chart-list/chart-list.component.ts
@@ -1,43 +1,35 @@
-import { Component, OnInit, Injector, ReflectiveInjector, ComponentFactoryResolver, ComponentFactory, ComponentRef } from '@angular/core';
-import { A2HighchartsGraphComponent } from '../a2-highcharts-graph/a2-highcharts-graph.component';
-import { GetTemperaturesService } from '../service/get-temperatures-service';
+import {Component, OnInit, ViewContainerRef,Injector, ComponentFactoryResolver, ReflectiveInjector, ComponentFactory, ViewChild} from '@angular/core';
+import {A2HighchartsGraphComponent} from '../a2-highcharts-graph/a2-highcharts-graph.component';
+import {GetTemperaturesService} from '../service/get-temperatures-service';
 
 @Component({
-  selector: 'app-chart-list',
-  templateUrl: './chart-list.component.html',
-  styleUrls: ['./chart-list.component.css']
+    selector: 'app-chart-list',
+    templateUrl: './chart-list.component.html',
+    styleUrls: ['./chart-list.component.css']
 })
 export class ChartListComponent implements OnInit {
 
-  chartList: Array<A2HighchartsGraphComponent>;
-  chartCompRefs: Array<ComponentRef<A2HighchartsGraphComponent>>;
-  chart = A2HighchartsGraphComponent;
-  myInjector: Injector;
-  factory: ComponentFactory<A2HighchartsGraphComponent>;
+    private getTemperaturesServiceInjector: Injector;
+    private factory: ComponentFactory<A2HighchartsGraphComponent>;
 
-  constructor(injector: Injector, private componentFactoryResolver: ComponentFactoryResolver) {
-    this.myInjector = ReflectiveInjector.resolveAndCreate([GetTemperaturesService], injector);
-    this.factory = this.componentFactoryResolver.resolveComponentFactory(A2HighchartsGraphComponent);
-    
-  }
+    @ViewChild('target', {read: ViewContainerRef}) target;
 
-  ngOnInit() {
-    this.chartList = new Array<A2HighchartsGraphComponent>();
-    this.chartList.push(this.factory.create(this.myInjector).instance);
-    this.chartList.push(this.factory.create(this.myInjector).instance);
-    this.chartList.push(this.factory.create(this.myInjector).instance);
-    this.chartList.push(this.factory.create(this.myInjector).instance);
+    constructor(private injector: Injector,
+                private componentFactoryResolver: ComponentFactoryResolver) {
+    }
 
-    this.chartCompRefs = new Array<ComponentRef<A2HighchartsGraphComponent>>();
+    appendChartComponent() {
+        this.getTemperaturesServiceInjector = ReflectiveInjector.resolveAndCreate([GetTemperaturesService], this.injector);
+        this.factory = this.componentFactoryResolver.resolveComponentFactory(A2HighchartsGraphComponent);
+        this.target.createComponent(this.factory, this.target.length, this.getTemperaturesServiceInjector)
+    }
 
-    // this.chart = A2HighchartsGraphComponent;
-  }
+    ngOnInit() {
+        this.appendChartComponent();
+    }
 
-  addChart() {
-    this.chartList.push(this.factory.create(this.myInjector).instance);
-    this.chartCompRefs.push(this.factory.create(this.myInjector));
-    this.chart = null;
-
-  }
+    addChart() {
+        this.appendChartComponent();
+    }
 
 }
