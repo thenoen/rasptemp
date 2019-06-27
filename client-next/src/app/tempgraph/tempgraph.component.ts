6f02363eccfc6834b26790f31d155b5512b1d938
marek <kordikmarek@gmail.com>
2019-06-27 20:07:22 +0200

    updated size of components, updated layout of tooltips, added temperature during last week,

--- client-next/src/app/tempgraph/tempgraph.component.ts
+++ client-next/src/app/tempgraph/tempgraph.component.ts
@@ -29,23 +29,24 @@ export class TempgraphComponent implements OnInit, AfterViewInit {
   legendPosition = "below";
 
   colorScheme = {
-    domain: ['#144aad','#03ff8e']
+    domain: ['#144aad', '#03ff8e']
   };
 
   data: any[] = [
     {
       name: '',
-      series: [{"name":"0", "value":"0"},{"name":"1", "value":"0"}]
+      series: [{"name": "0", "value": "0"}, {"name": "1", "value": "0"}]
     }
   ];
 
   currentTemperature: number;
 
-  @ViewChild('xxx',{static: false})
+  @ViewChild('xxx', {static: false})
   private xxx: ElementRef;
 
   constructor(private temperaturesService: GetTemperaturesService,
-              private el:ElementRef) {}
+              private el: ElementRef) {
+  }
 
   ngOnInit(): void {
     this.data[0].name = "Temperature during last " + this.hours + " hours";
@@ -54,9 +55,7 @@ export class TempgraphComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     console.log("ngAfterViewInit");
     this.view[0] = this.xxx.nativeElement.offsetWidth;
-    this.view[1] = this.xxx.nativeElement.offsetHeight;
-    // this.view[0] = 800;
-    this.view[1] = 300;
+    this.view[1] = this.view[0] * 0.8;
     console.log(this.view);
   }
 
@@ -67,7 +66,7 @@ export class TempgraphComponent implements OnInit, AfterViewInit {
       .then(() => this.data = [...this.data]);
 
     this.temperaturesService.getLastTemperature()
-      .then(value =>this.currentTemperature = value);
+      .then(value => this.currentTemperature = value);
   }
 
   public onSelect(data: any) {
