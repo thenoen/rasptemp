f21591469db493843ca11aa9a8ae7b0d011713c0
thenoen <kordikmarek@gmail.com>
2024-02-11 10:58:26 +0100

    incresed memory for latest java 11; added notes how to setup raspberry

--- server/configs-raspberry/start-prod.sh
+++ server/configs-raspberry/start-prod.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# For client compilation of client SWAP space has to be correctly configured - 2048 MB was sufficient.
+#
+#
+
 SYSTEM_PROPERTIES="
--Xms50m
--Xmx115m
--XX:MaxMetaspaceSize=50m
--XX:CompressedClassSpaceSize=1m"
+-Xms128m
+-Xmx256m
+-XX:MaxMetaspaceSize=100m
+-XX:CompressedClassSpaceSize=32m"
+
+PATH=/home/pi/.sdkman/candidates/java/current/bin/:$PATH # this is needed because CRON is using different PATH (crontab -e)
 
 java ${SYSTEM_PROPERTIES} -jar ../target/RaspTemp-Server-0.1-SNAPSHOT.jar --spring.config.location=application.properties
